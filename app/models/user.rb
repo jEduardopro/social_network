4d944cab0c28dcbbc0936fa156c20200347a863a
Jesus Eduardo Facio <lalo.codein22@gmail.com>
2023-11-11 14:15:01 -0600

    add validation test for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,18 @@
 class User < ApplicationRecord
+	include HasUuid
+
+	attr_accessor :password
+
+	validates :uuid, presence: true, uniqueness: true
+	validates :name, presence: true
+	validates :email, presence: true, uniqueness: true
+
+	def password=(password_str)
+		unless password_str.blank?
+			@password = password_str
+			self.password_salt = BCrypt::Engine.generate_salt
+			self.password_digest = BCrypt::Engine.hash_secret(@password, password_salt)
+		end
+	end
+
 end
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,11 @@
+describe User, type: :model do
+
+	subject { create(:user) }
+	describe 'validations' do
+		it { should validate_presence_of(:uuid) }
+		it { should validate_uniqueness_of(:uuid) }
+		it { should validate_presence_of(:name) }
+		it { should validate_presence_of(:email) }
+		it { should validate_uniqueness_of(:email) }
+	end
+end
