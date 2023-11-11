44e77908af1199457ab1260727099795fa40db00
Jesus Eduardo Facio <lalo.codein22@gmail.com>
2023-11-11 16:19:47 -0600

    add more validations for status model

--- app/models/status.rb
+++ app/models/status.rb
@@ -2,7 +2,7 @@ class Status < ApplicationRecord
 	include HasUuid
 
 	validates :uuid, presence: true, uniqueness: true
-	validates :body, presence: true
+	validates :body, presence: true, length: { minimum: 5, maximum: 140 }
 	validates :user_id, presence: true
 
 	belongs_to :user
--- spec/models/status_spec.rb
+++ spec/models/status_spec.rb
@@ -5,6 +5,8 @@ describe Status, type: :model do
 		it { should validate_presence_of(:uuid) }
 		it { should validate_uniqueness_of(:uuid) }
 		it { should validate_presence_of(:body) }
+		it { should validate_length_of(:body).is_at_least(5) }
+		it { should validate_length_of(:body).is_at_most(140) }
 		it { should validate_presence_of(:user_id) }
 	end
 
